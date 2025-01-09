8f79b8eb77b46f0da1e39b2d2b44926f7c5827ad
Alibek Jakupov <alibekjakupov@Alibeks-Laptop.local>
2025-01-09 18:06:37 +0100

    fix: summary azure

---
+++ backend/helpers/language_helper.py
@@ -0,0 +1,40 @@
+import os
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+from dotenv import load_dotenv
+from azure.ai.textanalytics import TextAnalyticsClient, ExtractiveSummaryAction
+
+
+def get_summary(document):
+    load_dotenv()
+    
+    key = os.environ.get('LANGUAGE_KEY')
+    endpoint = os.environ.get('LANGUAGE_ENDPOINT')
+
+    client = authenticate_client(key, endpoint)
+
+
+    poller = client.begin_analyze_actions(
+        document,
+        actions=[
+            ExtractiveSummaryAction(max_sentence_count=4)
+        ],
+    )
+
+    document_results = poller.result()
+
+    global_summary = ""
+
+    for result in document_results:
+        extract_summary_result = result[0]  # first document, first result
+        global_summary += " ".join([sentence.text for sentence in extract_summary_result.sentences])
+
+    return global_summary
+
+
+def authenticate_client(key, endpoint):
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,20 @@ async def analyze_document_content(file: UploadFile = File(...)):
     except UnicodeDecodeError:
         text = content.decode('latin-1')[:300]
     return {"text": text}  
-  
+
+@app.post("/summarize")
+async def chat(question: Question):
+    async def generate_response():
+        response = """
+        Elian was no ordinary boy. He lived in the quiet village of Oakhaven, nestled beside the Whispering Woods, a place most villagers avoided. Elian, however, was drawn to it. He felt a hum beneath the forest floor, a thrumming energy that resonated deep within him.
+        """
+        for chunk in response.split():
+            yield chunk + " "
+            await asyncio.sleep(0.1)  # simulate delay
+
+    return StreamingResponse(generate_response(), media_type="text/plain")
+
+
 @app.post("/chat")
 async def chat(question: Question):
     async def generate_response():
